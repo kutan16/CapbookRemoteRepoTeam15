b3139fd15d9414d276c67aee9bdbd49a9848efa8
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13098.corp.capgemini.com>
2019-04-03 18:44:50 +0530

    Exception Handling. javed 03-04-19 6.44

--- CapbookStore/src/main/java/com/cg/capbook/aspect/UserExceptionAspect.java
+++ CapbookStore/src/main/java/com/cg/capbook/aspect/UserExceptionAspect.java
@@ -4,12 +4,46 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cg.capbook.exceptions.EmailAlreadyExistException;
+import com.cg.capbook.exceptions.EmptyStatusException;
+import com.cg.capbook.exceptions.InvalidEmailException;
+import com.cg.capbook.exceptions.InvalidPasswordException;
+import com.cg.capbook.exceptions.PhoneNumberAlreadyLinkedException;
 import com.cg.capbook.exceptions.UserNotFoundException;
 
 @ControllerAdvice
 public class UserExceptionAspect {
 	@ExceptionHandler(UserNotFoundException.class)
-	public ModelAndView handelCustomerDetailsNotFoundException(Exception e) {
-		return new ModelAndView("findUserDetailsPage","errorMessage",e.getMessage());
+	public ModelAndView handelUserNotFoundException(Exception e) {
+		return new ModelAndView("errorPage","errorMessage",e.getMessage());
 	}
+
+
+@ExceptionHandler(InvalidPasswordException.class)
+public ModelAndView handelInvalidPasswordException(Exception e) {
+	return new ModelAndView("errorPage","errorMessage",e.getMessage());
 }
+
+@ExceptionHandler(EmptyStatusException.class)
+public ModelAndView handelEmptyStatusException(Exception e) {
+	return new ModelAndView("errorPage","errorMessage",e.getMessage());
+}
+
+@ExceptionHandler(InvalidEmailException.class)
+public ModelAndView handelInvalidEmailException(Exception e) {
+	return new ModelAndView("errorPage","errorMessage",e.getMessage());
+}
+
+@ExceptionHandler(PhoneNumberAlreadyLinkedException.class)
+public ModelAndView handelPhoneNumberAlreadylinkedException(Exception e) {
+	return new ModelAndView("errorPage","errorMessage",e.getMessage());
+}
+
+@ExceptionHandler(EmailAlreadyExistException.class)
+public ModelAndView handelEmailAlreadyExistException(Exception e) {
+	return new ModelAndView("errorPage","errorMessage",e.getMessage());
+}
+
+
+}
+
